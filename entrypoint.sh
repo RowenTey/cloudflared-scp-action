6704878e1b42c1d31e876f75c29d344d291168b5
Rowen <94209928+RowenTey@users.noreply.github.com>
2025-01-22 10:48:13 +0000

    feat: Added optional parameter for service token ID and secret

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,17 @@
 #!/bin/sh -l
 
 echo "Host $1" >> /root/.ssh/config
-echo "ProxyCommand cloudflared access ssh --hostname %h" >> /root/.ssh/config
+
+if [ -z $7 ] && [ -z $8 ]
+then
+    echo "ProxyCommand cloudflared access ssh --hostname %h" >> /root/.ssh/config
+else
+    echo "ProxyCommand cloudflared access ssh --hostname %h --id $7 --secret $8" >> /root/.ssh/config
+fi
+
 echo "$4" > /root/.ssh/key.pem
 chmod 600 /root/.ssh/key.pem
 
 ssh-keyscan $1 >> /root/.ssh/known_hosts
 
-scp -rp -i /root/.ssh/key.pem $5 $3@$1:$6
+scp -rp -i /root/.ssh/key.pem -o StrictHostKeyChecking=no $5 $3@$1:$6
